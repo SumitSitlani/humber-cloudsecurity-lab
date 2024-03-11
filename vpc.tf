099ec923456ba7d251f9abb24edc55c1acc58144
sumitsitlani <sumitsitlani@Sumits-MacBook-Pro-3.local>
2024-03-10 22:26:23 -0400

    formatted

--- vpc.tf
+++ vpc.tf
@@ -8,8 +8,8 @@ resource "aws_vpc" "security_lab_vpc" {
 }
 
 resource "aws_subnet" "security_lab_subnet" {
-  vpc_id     = aws_vpc.security_lab_vpc.id
-  cidr_block = var.subnet_cidr
+  vpc_id                  = aws_vpc.security_lab_vpc.id
+  cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
 }
 
